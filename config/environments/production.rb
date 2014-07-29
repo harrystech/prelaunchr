0c010b727ccbbc59178c2e84c0663e77be1f7ef2
Daniel Schwartz <dschwartz88@gmail.com>
2014-07-29 15:02:01 -0400

    adding precompile list to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Prelaunchr::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += %w( core.css bootstrap.css reset.css )
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
