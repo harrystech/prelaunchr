92d17191555b28d3d37fe6d2c25208e75a937d9e
C- Pres <cpres@c-pr.es>
2014-11-18 11:00:26 -0800

    CSV Export - adds referrer
    This make the CSV export much much more useful being that it will now have the referrer_id. Now you can just open the CSV in a spreadsheet or processing script and do all the calculations for user earnings.
    
    To note: CSV is the only exportable format that gives you 10,000 rows. Both JSON and XML are limited to 30, so CSV is the most important format to export all the details to.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,16 @@
+# Export to CSV with the referrer_id
+ActiveAdmin.register User do
+    csv do
+      column :id
+      column :email
+      column :referral_code
+      column :referrer_id
+      column :created_at
+      column :updated_at
+    end
+end
+
+# Display Default Index
 ActiveAdmin.register User do
   actions :index, :show
 end
