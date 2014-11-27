51324bbdc388309ecbbbcdbe3744df7999630c10
danny pham <pham.danny.t@gmail.com>
2014-11-26 21:35:16 -0800

    updates controller and spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,104 +1,74 @@
 class UsersController < ApplicationController
-    before_filter :skip_first_page, :only => :new
+  before_filter :skip_first_page, only: :new
+  before_filter :handle_ip, only: :create
 
-    def new
-        @bodyId = 'home'
-        @is_mobile = mobile_device?
+  def new
+    @bodyId = 'home'
+    @is_mobile = mobile_device?
 
-        @user = User.new
+    @user = User.new
 
-        respond_to do |format|
-            format.html # new.html.erb
-        end
+    respond_to do |format|
+      format.html # new.html.erb
     end
+  end
 
-    def create
-        # Get user to see if they have already signed up
-        @user = User.find_by_email(params[:user][:email]);
+  def create
+    @user = User.new(email: params[:user][:email])
+    @user.referrer = User.find_by_referral_code(cookies[:h_ref])
 
-        # If user doesnt exist, make them, and attach referrer
-        if @user.nil?
-
-            cur_ip = IpAddress.find_by_address(request.env['HTTP_X_FORWARDED_FOR'])
-
-            if !cur_ip
-                cur_ip = IpAddress.create(
-                    :address => request.env['HTTP_X_FORWARDED_FOR'],
-                    :count => 0
-                )
-            end
-
-            if cur_ip.count > 2
-                return redirect_to root_path
-            else
-                cur_ip.count = cur_ip.count + 1
-                cur_ip.save
-            end
-
-            @user = User.new(:email => params[:user][:email])
-
-            @referred_by = User.find_by_referral_code(cookies[:h_ref])
-
-            puts '------------'
-            puts @referred_by.email if @referred_by
-            puts params[:user][:email].inspect
-            puts request.env['HTTP_X_FORWARDED_FOR'].inspect
-            puts '------------'
-
-            if !@referred_by.nil?
-                @user.referrer = @referred_by
-            end
-
-            @user.save
-        end
-
-        # Send them over refer action
-        respond_to do |format|
-            if !@user.nil?
-                cookies[:h_email] = { :value => @user.email }
-                format.html { redirect_to '/refer-a-friend' }
-            else
-                format.html { redirect_to root_path, :alert => "Something went wrong!" }
-            end
-        end
+    if @user.save
+      cookies[:h_email] = { value: @user.email }
+      redirect_to '/refer-a-friend'
+    else
+      logger.info("Error saving user with email, #{params[:user][:email]}")
+      redirect_to root_path, alert: 'Something went wrong!'
     end
+  end
 
-    def refer
-        email = cookies[:h_email]
+  def refer
+    @bodyId = 'refer'
+    @is_mobile = mobile_device?
 
-        @bodyId = 'refer'
-        @is_mobile = mobile_device?
+    @user = User.find_by_email(cookies[:h_email])
 
-        @user = User.find_by_email(email)
-
-        respond_to do |format|
-            if !@user.nil?
-                format.html #refer.html.erb
-            else
-                format.html { redirect_to root_path, :alert => "Something went wrong!" }
-            end
-        end
+    respond_to do |format|
+      if !@user.nil?
+        format.html # refer.html.erb
+      else
+        format.html { redirect_to root_path, alert: 'Something went wrong!' }
+      end
     end
+  end
 
-    def policy
+  def policy
+  end
 
+  def redirect
+    redirect_to root_path, status: 404
+  end
+
+  private
+
+  def skip_first_page
+    if !Rails.application.config.ended
+      email = cookies[:h_email]
+      if email and !User.find_by_email(email).nil?
+        redirect_to '/refer-a-friend'
+      else
+        cookies.delete :h_email
+      end
     end
+  end
 
-    def redirect
-        redirect_to root_path, :status => 404
+  def handle_ip
+    return if !User.find_by_email(params[:user][:email]).nil?
+    @cur_ip = IpAddress.find_by_address(request.env['HTTP_X_FORWARDED_FOR'])
+    if @cur_ip.nil?
+      @cur_ip = IpAddress.create(
+        address: request.env['HTTP_X_FORWARDED_FOR'], count: 1)
+    else
+      return redirect_to root_path if @cur_ip.count > 2
     end
-
-    private
-
-    def skip_first_page
-        if !Rails.application.config.ended
-            email = cookies[:h_email]
-            if email and !User.find_by_email(email).nil?
-                redirect_to '/refer-a-friend'
-            else
-                cookies.delete :h_email
-            end
-        end
-    end
-
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,7 +21,7 @@ describe UsersController, type: :controller do
       post :create, user: {email: @email}
     end
 
-    it 'does not have a referrer when signing up unrefererred' do
+    it 'does not have a referrer when signing up unreferred' do
       user = assigns(:user)
       expect(user.referrer).to be_nil
     end
@@ -29,8 +29,16 @@ describe UsersController, type: :controller do
     it 'redirects to refer-a-friend on creation' do
       expect(response).to redirect_to '/refer-a-friend'
     end
+
+    it 'creates new IpAddress on new email submission' do
+      cur_ip = assigns(:cur_ip)
+      pending('still figuring out how to test ip address')
+      expect(cur_ip.count).to eq(1)
+      expect(cur_ip.address).to_not be_nil
+    end
   end
 
+  # probably should do more than redirect, but this is current behavior
   it 'redirects to main page when malformed email is submitted' do
     post :create, user: {email: 'notanemail'}
     expect(response).to redirect_to root_path
