e2612082c9baa94e6a10036a46e9cbb1fb51c1af
danny pham <pham.danny.t@gmail.com>
2014-11-26 20:10:43 -0800

    adds test for user saving with non email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ require 'users_helper'
 
 class User < ActiveRecord::Base
 
-    belongs_to :referrer, :class_name => "User", :foreign_key => "referrer_id"
-    has_many :referrals, :class_name => "User", :foreign_key => "referrer_id"
+    belongs_to :referrer, class_name: 'User', foreign_key: 'referrer_id'
+    has_many :referrals, class_name: 'User', foreign_key: 'referrer_id'
 
     attr_accessible :email
 
-    validates :email, :presence => true, :uniqueness => true, :format => { :with => /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i, :message => "Invalid email format." }
-    validates :referral_code, :uniqueness => true
+    validates :email, presence: true, uniqueness: true, format: { with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i, message: 'Invalid email format.' }
+    validates :referral_code, uniqueness: true
 
     before_create :create_referral_code
     after_create :send_welcome_email
@@ -16,27 +16,27 @@ class User < ActiveRecord::Base
     REFERRAL_STEPS = [
         {
             'count' => 5,
-            "html" => "Shave<br>Cream",
-            "class" => "two",
-            "image" =>  ActionController::Base.helpers.asset_path("refer/cream-tooltip@2x.png")
+            'html' => 'Shave<br>Cream',
+            'class' => 'two',
+            'image' =>  ActionController::Base.helpers.asset_path('refer/cream-tooltip@2x.png')
         },
         {
             'count' => 10,
-            "html" => "Truman Handle<br>w/ Blade",
-            "class" => "three",
-            "image" => ActionController::Base.helpers.asset_path("refer/truman@2x.png")
+            'html' => 'Truman Handle<br>w/ Blade',
+            'class' => 'three',
+            'image' => ActionController::Base.helpers.asset_path('refer/truman@2x.png')
         },
         {
             'count' => 25,
-            "html" => "Winston<br>Shave Set",
-            "class" => "four",
-            "image" => ActionController::Base.helpers.asset_path("refer/winston@2x.png")
+            'html' => 'Winston<br>Shave Set',
+            'class' => 'four',
+            'image' => ActionController::Base.helpers.asset_path('refer/winston@2x.png')
         },
         {
             'count' => 50,
-            "html" => "One Year<br>Free Blades",
-            "class" => "five",
-            "image" => ActionController::Base.helpers.asset_path("refer/blade-explain@2x.png")
+            'html' => 'One Year<br>Free Blades',
+            'class' => 'five',
+            'image' => ActionController::Base.helpers.asset_path('refer/blade-explain@2x.png')
         }
     ]
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 require 'securerandom'
 
-RSpec.describe User, :type => :model do
-  it "should error when saving without email" do
+RSpec.describe User, type: :model do
+  it 'should error when saving without email' do
     expect {
       user = User.new
       user.save!
     }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
-  it "should generate a referral code on creation" do
+  it 'should error when saving with malformed email' do
+    expect {
+      user = User.create!(email: 'notanemail')
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'should generate a referral code on creation' do
     user = User.new(email: 'user@example.com')
     user.run_callbacks(:create)
     expect(user).to receive(:create_referral_code)
@@ -17,7 +23,7 @@ RSpec.describe User, :type => :model do
     expect(user.referral_code).to_not be_nil
   end
 
-  it "should send a welcome email on save" do
+  it 'should send a welcome email on save' do
     user = User.new(email: 'user@example.com')
     user.run_callbacks(:create)
     expect(user).to receive(:send_welcome_email)
@@ -26,15 +32,15 @@ RSpec.describe User, :type => :model do
 end
 
 RSpec.describe UsersHelper do
-  describe "replace_if_collision" do
-    it "should return the same referral code if there is no collision" do
+  describe 'replace_if_collision' do
+    it 'should return the same referral code if there is no collision' do
       referral_code = SecureRandom.hex(5)
       @collision = nil
       candidate = UsersHelper.replace_if_collision(@collision, referral_code)
       expect(referral_code).to eq(candidate)
     end
 
-    it "should return a new referral code if there is a collision" do
+    it 'should return a new referral code if there is a collision' do
       referral_code = SecureRandom.hex(5)
       @collision = referral_code
       candidate = UsersHelper.replace_if_collision(@collision, referral_code)
