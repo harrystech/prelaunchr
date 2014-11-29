9052a8b73de04f6f56b0397890422ec90d25c075
danny pham <pham.danny.t@gmail.com>
2014-11-28 18:32:48 -0800

    fixes spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,54 +2,55 @@ require 'users_helper'
 
 class User < ActiveRecord::Base
 
-    belongs_to :referrer, class_name: 'User', foreign_key: 'referrer_id'
-    has_many :referrals, class_name: 'User', foreign_key: 'referrer_id'
+  belongs_to :referrer, class_name: 'User', foreign_key: 'referrer_id'
+  has_many :referrals, class_name: 'User', foreign_key: 'referrer_id'
 
-    attr_accessible :email
+  attr_accessible :email
 
-    validates :email, presence: true, uniqueness: true, format: { with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i, message: 'Invalid email format.' }
-    validates :referral_code, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i, message: 'Invalid email format.' }
+  validates :referral_code, uniqueness: true
 
-    before_create :create_referral_code
-    after_create :send_welcome_email
+  before_create :create_referral_code
+  after_create :send_welcome_email
 
-    REFERRAL_STEPS = [
-        {
-            'count' => 5,
-            'html' => 'Shave<br>Cream',
-            'class' => 'two',
-            'image' =>  ActionController::Base.helpers.asset_path('refer/cream-tooltip@2x.png')
-        },
-        {
-            'count' => 10,
-            'html' => 'Truman Handle<br>w/ Blade',
-            'class' => 'three',
-            'image' => ActionController::Base.helpers.asset_path('refer/truman@2x.png')
-        },
-        {
-            'count' => 25,
-            'html' => 'Winston<br>Shave Set',
-            'class' => 'four',
-            'image' => ActionController::Base.helpers.asset_path('refer/winston@2x.png')
-        },
-        {
-            'count' => 50,
-            'html' => 'One Year<br>Free Blades',
-            'class' => 'five',
-            'image' => ActionController::Base.helpers.asset_path('refer/blade-explain@2x.png')
-        }
-    ]
+  REFERRAL_STEPS = [
+    {
+      'count' => 5,
+      'html' => 'Shave<br>Cream',
+      'class' => 'two',
+      'image' =>  ActionController::Base.helpers.asset_path('refer/cream-tooltip@2x.png')
+    },
+    {
+      'count' => 10,
+      'html' => 'Truman Handle<br>w/ Blade',
+      'class' => 'three',
+      'image' => ActionController::Base.helpers.asset_path('refer/truman@2x.png')
+    },
+    {
+      'count' => 25,
+      'html' => 'Winston<br>Shave Set',
+      'class' => 'four',
+      'image' => ActionController::Base.helpers.asset_path('refer/winston@2x.png')
+    },
+    {
+      'count' => 50,
+      'html' => 'One Year<br>Free Blades',
+      'class' => 'five',
+      'image' => ActionController::Base.helpers.asset_path('refer/blade-explain@2x.png')
+    }
+  ]
 
-    private
+  private
 
-    def create_referral_code
-        referral_code = SecureRandom.hex(5)
-        @collision = User.find_by_referral_code(referral_code)
+  def create_referral_code
+    referral_code = SecureRandom.hex(5)
+    @collision = User.find_by_referral_code(referral_code)
 
-        self.referral_code = UsersHelper.replace_if_collision(@collision, referral_code)
-    end
+    self.referral_code = UsersHelper.replace_if_collision(
+      @collision, referral_code)
+  end
 
-    def send_welcome_email
-        UserMailer.delay.signup_email(self)
-    end
+  def send_welcome_email
+    UserMailer.delay.signup_email(self)
+  end
 end
