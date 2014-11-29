c7c75f005674ea88d1e3faac1d18c61e1ad7d6d1
danny pham <pham.danny.t@gmail.com>
2014-11-28 18:17:37 -0800

    reorgs and implements tests for new method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @bodyId = 'home'
     @is_mobile = mobile_device?
 
-    @user = User.new
+    @user = User.new # is this necessary?
 
     respond_to do |format|
       format.html # new.html.erb
@@ -14,14 +14,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(email: params[:user][:email])
-    @user.referrer = User.find_by_referral_code(cookies[:h_ref])
+    ref_code = cookies[:h_ref]
+    email = params[:user][:email]
+    @user = User.new(email: email)
+    @user.referrer = User.find_by_referral_code(ref_code) if ref_code
 
     if @user.save
       cookies[:h_email] = { value: @user.email }
       redirect_to '/refer-a-friend'
     else
-      logger.info("Error saving user with email, #{params[:user][:email]}")
+      logger.info("Error saving user with email, #{email}")
       redirect_to root_path, alert: 'Something went wrong!'
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,83 +6,101 @@ def generate_email
 end
 
 describe UsersController, type: :controller do
-  before(:each) do
-    @email = generate_email()
+  describe "new" do
+    it 'renders new user/landing page on first visit' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'skips first page on saved user\'s second visit' do
+      cookies[:h_email] = generate_email
+      expect(User).to receive(:find_by_email) { User.new }
+
+      get :new
+      expect(response).to redirect_to '/refer-a-friend'
+    end
   end
 
-  it 'has a referrer when referred' do
-    referrer_email = "#{SecureRandom.hex}@example.com"
-    referrer = User.create(email: referrer_email)
-    cookies[:h_ref] = referrer.referral_code
-
-    post :create, user: {email: @email}
-    user = assigns(:user)
-    expect(user.referrer.email).to eq(referrer.email)
-  end
-
-  context "no referrer" do
+  describe "saving users" do
     before(:each) do
-      ip_address = "192.0.2.#{SecureRandom.hex(3)}"
-      request.env['HTTP_X_FORWARDED_FOR'] = ip_address
+      @email = generate_email
+    end
+
+    it 'redirects to refer-a-friend on creation' do
       post :create, user: {email: @email}
+      expect(response).to redirect_to '/refer-a-friend'
+    end
+
+    it 'has a referrer when referred' do
+      referrer_email = "#{SecureRandom.hex}@example.com"
+      referrer = User.create(email: referrer_email)
+      cookies[:h_ref] = referrer.referral_code
+
+      post :create, user: {email: @email}
+      user = assigns(:user)
+      expect(user.referrer.email).to eq(referrer.email)
     end
 
     it 'does not have a referrer when signing up unreferred' do
+      post :create, user: {email: @email}
       user = assigns(:user)
       expect(user.referrer).to be_nil
     end
 
-    it 'redirects to refer-a-friend on creation' do
-      expect(response).to redirect_to '/refer-a-friend'
-    end
-  end
+    context 'ip addresses' do
+      before(:each) do
+        @ip_address = "192.0.2.#{SecureRandom.hex(3)}"
+        request.env['HTTP_X_FORWARDED_FOR'] = @ip_address
+        post :create, user: {email: @email}
+      end
 
-  context 'ip addresses' do
-    before(:each) do
-      @ip_address = "192.0.2.#{SecureRandom.hex(3)}"
-      request.env['HTTP_X_FORWARDED_FOR'] = @ip_address
-      post :create, user: {email: @email}
+      it 'creates a new IpAddress on new email submission' do
+        cur_ip = assigns(:cur_ip)
+        expect(cur_ip).to_not be_nil
+        expect(cur_ip.count).to eq(1)
+        expect(cur_ip.address).to_not be_nil
+      end
+
+      it 'increases the count of the IpAddress when then address appears again with same email' do
+        post :create, user: {email: @email}
+        cur_ip = assigns(:cur_ip)
+        expect(cur_ip).to_not be_nil
+        expect(cur_ip.count).to eq(2)
+      end
+
+      it 'increases the count of the IpAddress when then address appears again with different email' do
+        post :create, user: {email: generate_email()}
+        cur_ip = assigns(:cur_ip)
+        expect(cur_ip.count).to eq(2)
+      end
+
+      it 'redirects to the refer a friend page if the ip count is less than 3' do
+        post :create, user: {email: generate_email()}
+        cur_ip = assigns(:cur_ip)
+        expect(response).to redirect_to '/refer-a-friend'
+      end
+
+      it 'redirects to landing page when ip address has already appeared 3 times' do
+        post :create, user: {email: generate_email()}
+        post :create, user: {email: generate_email()}
+        post :create, user: {email: generate_email()} # 4th time
+        cur_ip = assigns(:cur_ip)
+        expect(cur_ip.count).to eq(3)
+        expect(response).to redirect_to('/')
+      end
+
+      it 'redirects user back to landing page when submitting email twice from different ip addresses' do
+        new_address = "192.0.2.#{SecureRandom.hex(3)}"
+        request.env['HTTP_X_FORWARDED_FOR'] = new_address
+        post :create, user: {email: @email}
+        expect(response).to redirect_to('/')
+      end
     end
 
-    it 'creates a new IpAddress on new email submission' do
-      cur_ip = assigns(:cur_ip)
-      expect(cur_ip).to_not be_nil
-      expect(cur_ip.count).to eq(1)
-      expect(cur_ip.address).to_not be_nil
+    # probably should do more than redirect, but this is current behavior
+    it 'redirects to main page when malformed email is submitted' do
+      post :create, user: {email: 'notanemail'}
+      expect(response).to redirect_to root_path
     end
-
-    it 'increases the count of the IpAddress when then address appears again with same email' do
-      post :create, user: {email: @email}
-      cur_ip = assigns(:cur_ip)
-      expect(cur_ip).to_not be_nil
-      expect(cur_ip.count).to eq(2)
-    end
-
-    it 'increases the count of the IpAddress when then address appears again with different email' do
-      post :create, user: {email: generate_email()}
-      cur_ip = assigns(:cur_ip)
-      expect(cur_ip.count).to eq(2)
-    end
-
-    it 'redirects to the refer a friend page if the ip count is less than 3' do
-      post :create, user: {email: generate_email()}
-      cur_ip = assigns(:cur_ip)
-      expect(response).to redirect_to '/refer-a-friend'
-    end
-
-    it 'redirects to landing page when ip address has already appeared 3 times' do
-      post :create, user: {email: generate_email()}
-      post :create, user: {email: generate_email()}
-      post :create, user: {email: generate_email()} # 4th time
-      cur_ip = assigns(:cur_ip)
-      expect(cur_ip.count).to eq(3)
-      expect(response).to redirect_to('/')
-    end
-  end
-
-  # probably should do more than redirect, but this is current behavior
-  it 'redirects to main page when malformed email is submitted' do
-    post :create, user: {email: 'notanemail'}
-    expect(response).to redirect_to root_path
   end
 end
