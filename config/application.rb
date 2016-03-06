569477c4d62477f55e4af3f620d2f083dc982b67
danny pham <pham.danny.t@gmail.com>
2016-03-06 10:23:19 -0800

    updates config

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,6 @@ module Prelaunchr
     config.assets.version = '1.0'
 
     # decides whether the prelaunch campaign has ended or not
-    config.ended = false
+    config.ended = ENV['CAMPAIGN_ENDED'].to_s == 'true'
   end
 end
